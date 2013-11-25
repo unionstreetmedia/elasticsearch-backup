1d68428c149504ebf05188ab41bd5ceabac88ce4
eborden <eborden@unionstmedia.com>
2013-11-24 20:18:49 -0500

    readme and host/port

---
+++ README.md
@@ -0,0 +1,7 @@
+=Elasticsearch backup
+
+Small utility for backing up Elasticsearch.
+
+```
+$ node build/src/index.js host port filePath [index [type]]]
+```
--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ var backup = require('./backup.js'),
     prom = require('./promiscuous-config.js');
 
 backup.run({
-    filePath: process.argv[2] || 'temp',
-    index: process.argv[3],
-    type: process.argv[4]
+    host: process.argv[2],
+    port: process.argv[3],
+    filePath: process.argv[4],
+    index: process.argv[5],
+    type: process.argv[6]
 });
