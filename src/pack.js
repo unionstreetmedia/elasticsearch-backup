2cf969f83651b244f88b2a5bacd52e3a1a921498
eborden <evan@evan-borden.com>
2013-11-26 11:06:18 -0500

    remove unnecessary lambda body

--- src/pack.js
+++ src/pack.js
@@ -15,12 +15,10 @@ module.exports = pack;
 
 //Curried function for writing data to a file stream
 function writeDocuments (fileStream) {
-    return data => {
-        return util.promiseWriteToFileStream(
-                fileStream,
-                _.map(data.hits.hits,
-                    data => JSON.stringify(data._source)).join('\n'));
-    };
+    return data => util.promiseWriteToFileStream(
+        fileStream,
+        _.map(data.hits.hits,
+            data => JSON.stringify(data._source)).join('\n'));
 }
 
 //Curried document getter
